debbf0471949ff4be59d2fd8b554621607f39d6f
Claudio Sotillos <claudiosotillosw@gmail.com>
2023-11-18 19:03:22 +0100

    Adding PoW

--- src/oml_reader_extension.cpp
+++ src/oml_reader_extension.cpp
@@ -6,11 +6,15 @@
 #include "duckdb/common/string_util.hpp"
 #include "duckdb/function/scalar_function.hpp"
 #include "duckdb/main/extension_util.hpp"
+#include "duckdb/catalog/catalog.hpp"
 #include <duckdb/parser/parsed_data/create_scalar_function_info.hpp>
 #include <duckdb/parser/parsed_data/create_table_function_info.hpp>
+#include "duckdb/parser/parsed_data/create_sequence_info.hpp"
+#include "duckdb/parser/parsed_data/create_table_info.hpp"
+#include "duckdb/parser/parsed_data/create_view_info.hpp"
+#include "duckdb/parser/constraints/not_null_constraint.hpp"
 #include <fstream>
-// #include <sstream>
-// #include <vector>
+#include <cassert>
 #include <iostream>
 
 
@@ -18,9 +22,11 @@
 #include "oml_parser.hpp"
 
 
-
 namespace duckdb
-{
+{   
+    ////////////////////////////////////////////
+    //      OmlGen  Functions                //
+    ///////////////////////////////////////////
     inline void OMLScanInternal(ClientContext &context, TableFunctionInput &data_p, DataChunk &output){
         auto &global_data = data_p.global_state->Cast<GlobalState>();
         auto &bind_data = data_p.bind_data->Cast<BindState>();
@@ -77,28 +83,101 @@ namespace duckdb
 
 
 
+    ////////////////////////////////////////////
+    //    Power_Consumption_load  Functions  //
+    ///////////////////////////////////////////
+    
+    inline void POWScanInternal(ClientContext &context, TableFunctionInput &data_p, DataChunk &output){
+        auto &global_data = data_p.global_state->Cast<GlobalState>();
+        auto &bind_data = data_p.bind_data->Cast<BindState>();
+        idx_t actual_row = 0;
 
-    inline void OMLScalarFunInternal(DataChunk &args, ExpressionState &state, Vector &result) {
-        auto &name_vector = args.data[0];
-        UnaryExecutor::Execute<string_t, string_t>(
-            name_vector, result, args.size(),
-            [&](string_t name) {
-                return StringVector::AddString(result, "OML "+name.GetString()+" 🐥");;
-            });
+        // auto &catalog = Catalog::GetCatalog(context, "");
+        // auto &tbl_catalog = catalog.GetEntry<TableCatalogEntry>(context, bind_data.schema, bind_data.table);
+
+        for (size_t i = global_data.rows_read; i < bind_data.fileRows.size(); ++i) {
+            if (actual_row >= STANDARD_VECTOR_SIZE ){
+                break;
+            }
+            std::istringstream iss(bind_data.fileRows[i]);
+            std::vector<std::string> tokens;
+
+            // break the row into a list of numbers
+            do {
+                std::string token;
+                iss >> token;
+                if (!token.empty()) {
+                    tokens.push_back(token);
+                }
+            } while (iss);
+
+            
+            // Going over the row values (their idx (j) corresponds to the column idx)
+            for (size_t j = 0; j < tokens.size(); j++) {
+                duckdb::Value duckVal(tokens.at(j));
+                output.SetValue(j, actual_row, duckVal);
+            }
+            actual_row ++; 
+            global_data.rows_read++;
+        }
+        output.SetCardinality(actual_row);  
     }
 
+
+
+    unique_ptr<FunctionData> POWScanBind(ClientContext &context, TableFunctionBindInput &input, vector<LogicalType> &return_types, vector<string> &names){
+        auto file = input.inputs[0].ToString();
+        auto result = make_uniq<BindState>();
+        OMLParser parser(return_types, names, file);
+        names = parser.GetNames();
+
+        // Check if the file has as many columns as the table we are going to create
+        // 
+        if (!(names.size() == 8)) {
+            std::cerr << "Assertion failed: The file that you are inputing doesn't have 8 columns." << std::endl;
+            std::cerr << "Actual Amount of columns: " << names.size() << std::endl;
+            // assert(names.size() == 8);
+            D_ASSERT(names.size() == 8) ;            
+        }
+    
+        return_types = parser.GetReturnTypes();
+
+        auto info = make_uniq<CreateTableInfo>();
+        info->schema = "main";
+        info->table =  "Power_Consumption";
+        info->on_conflict = OnCreateConflict::IGNORE_ON_CONFLICT;
+        info->temporary = false;
+        names = {"experiment_id", "node_id", "node_id_seq", "time_sec", "time_usec", "power", "current", "voltage"};
+        vector<bool> not_null_constraint = {false, false, false, true, true, true, true, true};
+        for (idx_t i = 0; i < return_types.size(); i++) {
+            info->columns.AddColumn(ColumnDefinition(names[i], return_types[i]));
+            if (not_null_constraint.size() != 0 && not_null_constraint[i])
+                info->constraints.push_back(make_uniq<NotNullConstraint>(LogicalIndex(i)));
+        }
+        auto &catalog = Catalog::GetCatalog(context, "");
+        catalog.CreateTable(context, std::move(info));
+
+    
+        result->fileRows =  parser.GetRows();
+        return std::move(result);
+    }
+
+
+    ////////////////////////////////////////////
+    //          Loading All Functions         //
+    ///////////////////////////////////////////
     
     static void LoadInternal(DatabaseInstance &instance)
     {   
         // Register all table functions
         //  OMLScanBind -- Used to create the table Schema based on the metadata of the input file. The reading is done by the Oml Parser class.
         //  OMLScanInternal -- Loads the tuples (rows) of the file into the already defined table.
-        auto oml_reader_fun= TableFunction("oml_reader", {LogicalType::VARCHAR}, OMLScanInternal ,OMLScanBind,OMLScanInitGlobal);
-        ExtensionUtil::RegisterFunction(instance, oml_reader_fun);
+        auto oml_gen= TableFunction("OmlGen", {LogicalType::VARCHAR}, OMLScanInternal ,OMLScanBind,OMLScanInitGlobal);
+        ExtensionUtil::RegisterFunction(instance, oml_gen);
 
 
-        auto oml_reader_scalar= ScalarFunction("oml", {LogicalType::VARCHAR}, LogicalType::VARCHAR, OMLScalarFunInternal);
-        ExtensionUtil::RegisterFunction(instance, oml_reader_scalar);
+        auto oml_POW= TableFunction("Power_Consumption_load", {LogicalType::VARCHAR}, POWScanInternal ,POWScanBind, OMLScanInitGlobal);
+        ExtensionUtil::RegisterFunction(instance, oml_POW);
     }
 
     void OmlReaderExtension::Load(DuckDB &db)
